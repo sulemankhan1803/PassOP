bb44ddd7f1323b2aa09b2051c04a4df8de07b0c1
Mohammed Suleman Khan <sulemankhan1803@gmail.com>
2025-06-05 15:32:52 +0530

    Update layout to keep footer at bottom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,13 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-      <Manager />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-1 bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+          <Manager />
+        </div>
+        <Footer />
       </div>
-      
-      <Footer />
     </>
   );
 }
